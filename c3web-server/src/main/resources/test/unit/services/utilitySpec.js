341079c41c2eb13aba00cc6826888263faea0a9f
Valentin Korolev <kor.val@inbox.ru>
2015-12-19 03:15:26 +0300

    [added] small utility service and example test-spec

--- c3web-server/src/main/resources/test/unit/services/utilitySpec.js
+++ c3web-server/src/main/resources/test/unit/services/utilitySpec.js
@@ -1,14 +1,32 @@
-//'use strict';
+'use strict';
 
-describe('utility', function() {
-	var utility;
+describe('utility', function () {
+    var _, utility;
 
-  	beforeEach(module('C3web.services'));
-  	
-  	// beforeEach(inject());
+    beforeEach(module('C3web.services'));
 
-  	// Test service availability
-  	it('should inject utiliity service', inject(function(utility){
-  		expect(utility).toBeDefined();
-  	}));
+    beforeEach(inject(['utility',
+        function (u) {
+            utility = u;
+        }
+    ]));
+
+    // Test service availability
+    it('should inject utiliity service', function () {
+        expect(utility).toBeDefined();
+    });
+
+    describe('equalsIgnoreCase', function () {
+        it('should be true when strings are empty', function () {
+            expect(utility.equalsIgnoreCase(null, null)).toBeTruthy();
+        });
+
+        it('should be true when strings are in different cases', function () {
+            expect(utility.equalsIgnoreCase('str', 'STR')).toBeTruthy();
+        });
+
+        it('should be false when strings are not equal', function(){
+            expect(utility.equalsIgnoreCase('str', 'strl')).toBeFalsy();
+        });
+    })
 });
---
+++ c3web-server/src/main/resources/web/scripts/services/utility.js
@@ -0,0 +1,13 @@
+(function (angular, undefined) {
+    var service = function () {
+        return {
+            equalsIgnoreCase: function (s1, s2) {
+                return (!s1 && !s2) || s1.toLowerCase() === s2.toLowerCase();
+            }
+        };
+    };
+
+    angular.module('C3web.services')
+        .factory('utility', service);
+
+})(angular)
